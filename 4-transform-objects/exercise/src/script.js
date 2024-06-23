21b75a0fe2f457f94ccfb01d14f6e5725a12ad91
Celine Macke <cmacke121@gmail.com>
2024-06-23 19:28:40 -0400

    understanding basic transformations

--- 4-transform-objects/exercise/src/script.js
+++ 4-transform-objects/exercise/src/script.js
@@ -9,10 +9,61 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
+/*
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0x8E96F2 })
 const mesh = new THREE.Mesh(geometry, material)
+
+//mesh.position.x = 0.7;
+//mesh.position.y = -0.6;
+//mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
 scene.add(mesh)
+
+//Scale
+//mesh.scale.x = 2
+//mesh.scale.y = 0.5
+//mesh.scale.z = 0.5
+//mesh.scale.set(2, 0.5, 0.5);
+
+//Rotation
+mesh.rotation.reorder('YXZ');
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+*/
+
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial( {color: 0x70DDF3} )
+)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial( {color: 0x8E96F2} )
+)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial( {color: 0xD7BFF6} )
+)
+
+cube2.position.set(-2, 0, 0);
+cube3.position.set(2, 0, 0);
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+//Axes helper 
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -26,8 +77,9 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.z = 3;
 scene.add(camera)
+
 
 /**
  * Renderer
