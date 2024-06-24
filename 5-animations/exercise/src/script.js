ffed71cc93c517e7ad9cb8ec2187eeb9d71265af
Celine Macke <cmacke121@gmail.com>
2024-06-23 21:40:23 -0400

    end of lesson 5

--- 5-animations/exercise/src/script.js
+++ 5-animations/exercise/src/script.js
@@ -1,16 +1,35 @@
-import * as THREE from 'three'
-
+import * as THREE from 'three';
+import gsap from 'gsap';
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+const group = new THREE.Group();
+scene.add(group);
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// Objects
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial( {color: 0x70DDF3} )
+)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial( {color: 0x8E96F2} )
+)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial( {color: 0xD7BFF6} )
+)
+cube2.position.set(-2, 0, 0);
+cube3.position.set(2, 0, 0);
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+
 
 // Sizes
 const sizes = {
@@ -28,4 +47,33 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+//Clock
+const clock = new THREE.Clock(); 
+
+//gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
+//gsap.to(mesh.position, {duration: 1, delay: 2, x: 0})
+
+//Animations
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    //update mesh
+    //camera.position.y = Math.sin(elapsedTime);
+    //camera.position.x = Math.cos(elapsedTime);
+    //camera.lookAt(mesh.position);
+    cube1.rotation.x = elapsedTime * Math.PI * 0.25;
+    cube1.rotation.y = elapsedTime * Math.PI * 0.25;
+
+    cube1.position.y = Math.sin(elapsedTime);
+    cube2.position.y = -Math.sin(elapsedTime);
+    cube3.position.y = Math.cos(elapsedTime);
+    cube2.rotation.x = elapsedTime * Math.PI * 0.25;
+    cube3.rotation.y = elapsedTime * Math.PI * 0.25;
+
+    //Render
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+}
+
+tick();
